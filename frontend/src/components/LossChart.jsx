555d5e5cb427f8f8ba3a472702eafbcef1e8d0b1
owen <owen.lee2406@gmail.com>
2025-07-10 16:40:35 -0700

    added labels

--- frontend/src/components/LossChart.jsx
+++ frontend/src/components/LossChart.jsx
@@ -1,6 +1,8 @@
 import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 
 const LossChart = ({ data }) => (
+    <div style={{ textAlign: 'center', color: 'black'}}>
+    <h3 style={{ marginBottom: '0.5rem' }}>Loss</h3>
   <LineChart width={400} height={250} data={data}>
     <CartesianGrid strokeDasharray="3 3" />
     <XAxis dataKey="epoch" />
@@ -8,6 +10,7 @@ const LossChart = ({ data }) => (
     <Tooltip />
     <Line type="monotone" dataKey="loss" stroke="#8884d8" dot={false} />
   </LineChart>
+  </div>
 );
 
 export default LossChart;
